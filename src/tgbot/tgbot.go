7f78da29b3dc24b8b16f26bbe017515abb816de2
Roman Martynov <sintanial@gmail.com>
2021-01-13 15:55:55 +0800

    fix formatting

--- src/tgbot/tgbot.go
+++ src/tgbot/tgbot.go
@@ -219,8 +219,8 @@ func (self *TgBot) OnRequestCalc(update tgbotapi.Update, data string) {
 	replyText += fmt.Sprintf("Для открытия позиции \n\n")
 	replyText += fmt.Sprintf("Объём средств: *$%.2f*\n\n", positionVolume)
 	replyText += fmt.Sprintf("Кол-во токенов (с риском *%.2f%%*):\n\n", risk)
-	replyText += fmt.Sprintf("При цене входа *$%.2f*:  *%v* `%.4f`\n", offer.MaxRangePrice, offer.CryptoCode, minTokenVolume*risk)
-	replyText += fmt.Sprintf("При цене входа *$%.2f*:  *%v* `%.4f`", offer.MinRangePrice, offer.CryptoCode, maxTokenVolume*risk)
+	replyText += fmt.Sprintf("При цене входа *$%.2f*:\n*%v* `%.4f`\n\n", offer.MaxRangePrice, offer.CryptoCode, minTokenVolume*risk)
+	replyText += fmt.Sprintf("При цене входа *$%.2f*:\n*%v* `%.4f`", offer.MinRangePrice, offer.CryptoCode, maxTokenVolume*risk)
 
 	self.sendTgMessage(update.Message.Chat.ID, replyText)
 }
